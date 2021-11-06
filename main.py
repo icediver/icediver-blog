5cea8eb1d82d8826dd1fff62c2c17389fc3508d6
IceDiver <admin@example.com>
2021-11-07 01:14:19 +0300

    Change env

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 print()
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # <editor-fold desc="Шаг 1 нужно настроить приложение Flask для использования Flask_Login.">
